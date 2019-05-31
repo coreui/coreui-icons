3f5ebeab83c9496ea701a19e1f4f7a271eb357d2
woothu <32914662+woothu@users.noreply.github.com>
2019-05-31 10:34:45 +0200

    feat: add social icons to free varsion

--- js/index.js
+++ js/index.js
@@ -2,185 +2,191 @@ import { icons } from './iconsGenerated.js'
 export default icons 
 
 
-import { accountLogout } from './accountLogout.js'
-import { actionUndo } from './actionUndo.js'
 import { actionRedo } from './actionRedo.js'
+import { accountLogout } from './accountLogout.js'
 import { alignCenter } from './alignCenter.js'
-import { alignLeft } from './alignLeft.js'
+import { actionUndo } from './actionUndo.js'
 import { alignRight } from './alignRight.js'
+import { alignLeft } from './alignLeft.js'
 import { arrowBottom } from './arrowBottom.js'
 import { arrowLeft } from './arrowLeft.js'
-import { arrowRight } from './arrowRight.js'
 import { arrowTop } from './arrowTop.js'
+import { arrowRight } from './arrowRight.js'
 import { ban } from './ban.js'
-import { bell } from './bell.js'
 import { basketLoaded } from './basketLoaded.js'
-import { bold } from './bold.js'
 import { bookmark } from './bookmark.js'
+import { bell } from './bell.js'
+import { bold } from './bold.js'
 import { briefcase } from './briefcase.js'
-import { britishPound } from './britishPound.js'
 import { brush } from './brush.js'
+import { britishPound } from './britishPound.js'
 import { calculator } from './calculator.js'
-import { calendar } from './calendar.js'
-import { cart } from './cart.js'
 import { chart } from './chart.js'
+import { cart } from './cart.js'
+import { calendar } from './calendar.js'
 import { check } from './check.js'
-import { chevronBottom } from './chevronBottom.js'
-import { chevronLeft } from './chevronLeft.js'
 import { chevronRight } from './chevronRight.js'
+import { chevronLeft } from './chevronLeft.js'
+import { chevronBottom } from './chevronBottom.js'
 import { chevronTop } from './chevronTop.js'
 import { cloudDownload } from './cloudDownload.js'
-import { cloudUpload } from './cloudUpload.js'
 import { cloud } from './cloud.js'
+import { cloudUpload } from './cloudUpload.js'
 import { code } from './code.js'
-import { commentSquare } from './commentSquare.js'
 import { creditCard } from './creditCard.js'
 import { cursor } from './cursor.js'
+import { commentSquare } from './commentSquare.js'
 import { dollar } from './dollar.js'
 import { envelopeClosed } from './envelopeClosed.js'
 import { envelopeLetter } from './envelopeLetter.js'
 import { envelopeOpen } from './envelopeOpen.js'
 import { euro } from './euro.js'
 import { file } from './file.js'
-import { globe } from './globe.js'
 import { graph } from './graph.js'
+import { globe } from './globe.js'
 import { home } from './home.js'
 import { inbox } from './inbox.js'
 import { info } from './info.js'
 import { italic } from './italic.js'
+import { justifyRight } from './justifyRight.js'
 import { justifyCenter } from './justifyCenter.js'
 import { justifyLeft } from './justifyLeft.js'
-import { justifyRight } from './justifyRight.js'
 import { laptop } from './laptop.js'
 import { layers } from './layers.js'
 import { lightbulb } from './lightbulb.js'
+import { lockLocked } from './lockLocked.js'
 import { list } from './list.js'
 import { locationPin } from './locationPin.js'
-import { lockLocked } from './lockLocked.js'
 import { lockUnlocked } from './lockUnlocked.js'
-import { magnifyingGlass } from './magnifyingGlass.js'
 import { map } from './map.js'
 import { monitor } from './monitor.js'
+import { magnifyingGlass } from './magnifyingGlass.js'
 import { moon } from './moon.js'
-import { note } from './note.js'
 import { options } from './options.js'
-import { paperclip } from './paperclip.js'
+import { note } from './note.js'
 import { pencil } from './pencil.js'
-import { people } from './people.js'
 import { phone } from './phone.js'
+import { paperclip } from './paperclip.js'
 import { print } from './print.js'
+import { people } from './people.js'
 import { puzzle } from './puzzle.js'
 import { rss } from './rss.js'
 import { screenDesktop } from './screenDesktop.js'
 import { screenSmartphone } from './screenSmartphone.js'
 import { settings } from './settings.js'
-import { share } from './share.js'
+import { socialFacebook } from './socialFacebook.js'
+import { socialLinkedin } from './socialLinkedin.js'
 import { shield } from './shield.js'
+import { share } from './share.js'
+import { socialTwitter } from './socialTwitter.js'
 import { sortAscending } from './sortAscending.js'
 import { sortDescending } from './sortDescending.js'
 import { speech } from './speech.js'
-import { speedometer } from './speedometer.js'
 import { star } from './star.js'
-import { sun } from './sun.js'
+import { speedometer } from './speedometer.js'
 import { tablet } from './tablet.js'
 import { tags } from './tags.js'
-import { thumbDown } from './thumbDown.js'
+import { sun } from './sun.js'
 import { thumbUp } from './thumbUp.js'
+import { thumbDown } from './thumbDown.js'
 import { trash } from './trash.js'
 import { underline } from './underline.js'
 import { userFemale } from './userFemale.js'
 import { userFollow } from './userFollow.js'
-import { userUnfollow } from './userUnfollow.js'
 import { user } from './user.js'
+import { userUnfollow } from './userUnfollow.js'
 import { yen } from './yen.js'
-export { accountLogout }
-export { actionUndo }
 export { actionRedo }
+export { accountLogout }
 export { alignCenter }
-export { alignLeft }
+export { actionUndo }
 export { alignRight }
+export { alignLeft }
 export { arrowBottom }
 export { arrowLeft }
-export { arrowRight }
 export { arrowTop }
+export { arrowRight }
 export { ban }
-export { bell }
 export { basketLoaded }
-export { bold }
 export { bookmark }
+export { bell }
+export { bold }
 export { briefcase }
-export { britishPound }
 export { brush }
+export { britishPound }
 export { calculator }
-export { calendar }
-export { cart }
 export { chart }
+export { cart }
+export { calendar }
 export { check }
-export { chevronBottom }
-export { chevronLeft }
 export { chevronRight }
+export { chevronLeft }
+export { chevronBottom }
 export { chevronTop }
 export { cloudDownload }
-export { cloudUpload }
 export { cloud }
+export { cloudUpload }
 export { code }
-export { commentSquare }
 export { creditCard }
 export { cursor }
+export { commentSquare }
 export { dollar }
 export { envelopeClosed }
 export { envelopeLetter }
 export { envelopeOpen }
 export { euro }
 export { file }
-export { globe }
 export { graph }
+export { globe }
 export { home }
 export { inbox }
 export { info }
 export { italic }
+export { justifyRight }
 export { justifyCenter }
 export { justifyLeft }
-export { justifyRight }
 export { laptop }
 export { layers }
 export { lightbulb }
+export { lockLocked }
 export { list }
 export { locationPin }
-export { lockLocked }
 export { lockUnlocked }
-export { magnifyingGlass }
 export { map }
 export { monitor }
+export { magnifyingGlass }
 export { moon }
-export { note }
 export { options }
-export { paperclip }
+export { note }
 export { pencil }
-export { people }
 export { phone }
+export { paperclip }
 export { print }
+export { people }
 export { puzzle }
 export { rss }
 export { screenDesktop }
 export { screenSmartphone }
 export { settings }
-export { share }
+export { socialFacebook }
+export { socialLinkedin }
 export { shield }
+export { share }
+export { socialTwitter }
 export { sortAscending }
 export { sortDescending }
 export { speech }
-export { speedometer }
 export { star }
-export { sun }
+export { speedometer }
 export { tablet }
 export { tags }
-export { thumbDown }
+export { sun }
 export { thumbUp }
+export { thumbDown }
 export { trash }
 export { underline }
 export { userFemale }
 export { userFollow }
-export { userUnfollow }
 export { user }
+export { userUnfollow }
 export { yen }
